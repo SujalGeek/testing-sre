f85bf4170e02dce237d7ab6bfdfd565409c46573
SujalGeek <morwanisujal875@gmail.com>
2026-03-30 16:21:51 +0530

    working-branch-done

---
+++ sre-remediation-service/src/main/java/com/example/sre_remediation_service/SreRemediationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.sre_remediation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreRemediationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreRemediationServiceApplication.class, args);
+	}
+
+}
---
+++ sre-remediation-service/src/test/java/com/example/sre_remediation_service/SreRemediationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sre_remediation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SreRemediationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
